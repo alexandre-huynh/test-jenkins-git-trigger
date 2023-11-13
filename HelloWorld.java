f6603ff0bd7456e0a6212d4d16f8c59530743561
Alexandre Huynh <99390277+alexandre-huynh@users.noreply.github.com>
2023-11-13 10:31:17 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world! Changes have been made. Testing"); 
+        System.out.println("Hello world! Changes have been made. Testing Jenkins pipelines"); 
     }
 }
